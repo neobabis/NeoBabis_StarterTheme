7f64de89db71c839d2c57cb99f7741a86d5aed50
neobabis <48261152+neobabis@users.noreply.github.com>
2020-12-10 01:35:24 +0200

    Update functions-core.php

--- inc/core/functions-core.php
+++ inc/core/functions-core.php
@@ -25,7 +25,7 @@ function BASICTheme_Core_setup(){
     add_theme_support( 'title-tag' );
     // Add default posts and comments RSS feed links to head.
     add_theme_support( 'automatic-feed-links' );
-	  // Switch default core markup for search form, comment form, and comments to output valid HTML5.
+	// Switch default core markup for search form, comment form, and comments to output valid HTML5.
     add_theme_support( 'html5', array( 'comment-form', 'comment-list', 'search-form', 'gallery', 'caption' ) );
     // Registers theme support for Responsive embedded content
     add_theme_support( 'responsive-embeds' );
@@ -57,12 +57,55 @@ function BASICTheme_Core_setup(){
 
     add_filter( 'the_generator', '__return_false' );
     add_filter( 'show_admin_bar','__return_false' );
-
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-
 }
 
+
+/**
+* Disable the emojis
+*/
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+* Filter function used to remove the tinymce emoji plugin.
+* 
+* @param array $plugins 
+* @return array Difference betwen the two arrays
+*/
+function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+
+/**
+* Remove emoji CDN hostname from DNS prefetching hints.
+*
+* @param array $urls URLs to print for resource hints.
+* @param string $relation_type The relation type the URLs are printed for.
+* @return array Difference betwen the two arrays.
+*/
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+    if ( 'dns-prefetch' == $relation_type ) {
+        /** This filter is documented in wp-includes/formatting.php */
+        $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+        $urls = array_diff( $urls, array( $emoji_svg_url ) );
+    }
+
+    return $urls;
+}
 
 /**
  * Create Message when username or password is incorrect!
