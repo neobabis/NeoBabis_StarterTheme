d7d59913177ad403f273a68a2db11d7a358f47c9
neobabis <48261152+neobabis@users.noreply.github.com>
2021-01-07 22:06:56 +0200

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -4,10 +4,9 @@
  * Include all the sidebars you want to display with the get_sidebar(); function
  * @package NeoBabis_StarterTheme
  */
-?>
 
-<?php if ( is_active_sidebar( 'SidebarName' ) ) : ?>
+if ( is_active_sidebar( 'SidebarName' ) ) : ?>
   <div id="SidebarName">
     <?php dynamic_sidebar( 'SidebarName' ); ?>
   </div>
-<?php endif; ?>
+<?php endif;
