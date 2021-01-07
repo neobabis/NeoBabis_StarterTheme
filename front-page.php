eaa56c763e10ff5d3f5b836d8da7152b517c6154
neobabis <48261152+neobabis@users.noreply.github.com>
2021-01-07 22:05:16 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -3,9 +3,11 @@
  * Front Page if I want a different front page of blog posts
  * @package NeoBabis_StarterTheme
  */
+
+get_header();
 ?>
-<?php get_header(); ?>
 
   <h1>================= FRONT-PAGE.php =================</h1>
 
-<?php get_footer(); ?>
+<?php
+get_footer();
