bda116739b46fb18a71d35991b091ebb16865590
neobabis <48261152+neobabis@users.noreply.github.com>
2021-01-07 22:06:24 +0200

    Update page.php

--- page.php
+++ page.php
@@ -3,11 +3,12 @@
  * Page Template render a static page
  * @package NeoBabis_StarterTheme
  */
-?>
 
-<?php get_header(); ?>
+get_header();
+?>
 
   <h1>================= PAGE.php =================</h1>
   <!-- the_content(); -->
 
-<?php get_footer(); ?>
+<?php
+get_footer();
