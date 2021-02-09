c1141b70e9be3700dac84e21fa305d66582ec6b8
neobabis <48261152+neobabis@users.noreply.github.com>
2021-02-09 12:37:30 +0200

    Update page.php

--- page.php
+++ page.php
@@ -8,7 +8,13 @@ get_header();
 ?>
 
   <h1>================= PAGE.php =================</h1>
-  <!-- the_content(); -->
+  <div class="container">
+      <div class="row">
+          <div class="col-md-12">
+              <?php the_content(); ?>
+          </div>
+      </div>
+  </div>
 
 <?php
 get_footer();
