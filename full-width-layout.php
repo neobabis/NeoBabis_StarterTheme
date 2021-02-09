641302fe52cfe4a924df565801952b302e7d7fd9
neobabis <48261152+neobabis@users.noreply.github.com>
2021-02-09 12:38:14 +0200

    Add files via upload

---
+++ full-width-layout.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Template Name: Full-width layout
+ * Template Post Type: post, page
+ * @package NeobabisTheme
+ */
+
+get_header();
+?>
+
+<h1>================= FULL-WIDTH-LAYOUT.php =================</h1>
+
+<?php the_content(); ?>
+
+<?php
+get_footer();
